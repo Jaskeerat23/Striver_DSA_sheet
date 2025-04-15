32fc4b8d1252f896d907b4713b8fe402cf6c7359
Jaskeerat23 <jaskeerat23006@gmail.com>
2025-04-15 21:07:36 +0530

    15/4/25

--- Arrays/nextPermutation.cpp
+++ Arrays/nextPermutation.cpp
@@ -8,23 +8,32 @@ class Solution{
 public:
     static void nextPermutation(vector<int>& nums) {
         int brkIdx = nums.size()-1, max = brkIdx, size = nums.size()-1;
+
+        //Finding the Brakpoint
         for(int i = nums.size()-2; i>-1; i--)
             if(nums[i] < nums[i+1]){
                 brkIdx = i;
                 break;
             }
-        cout << "Break index is : " << brkIdx << endl;
+        
+        //If BreakPoint does not exist such that array is sorted
         if(brkIdx == size){
             reverse(nums.begin(), nums.end());
             return;
         }
+
+        //Finding the smallest grater element
         for(int i = brkIdx+1; i <= size; i++){
             if(nums[i] > nums[brkIdx])
                 max = i;
             if(nums[i] > nums[brkIdx] && nums[i] < nums[max])
                 max = i;
         }
+
+        //Swapping BreakPoint and smallest greater element
         swap(nums[brkIdx], nums[max]);
+
+        //Reversing rest of the array from BreakPoint + 1 to end
         reverse(nums.begin()+brkIdx+1, nums.end());
     }
 };
