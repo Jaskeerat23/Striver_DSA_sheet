6ec05804911aa81d12fa8fc1bdfd0ae3d93542f5
Jaskeerat23 <jaskeerat23006@gmail.com>
2025-05-08 19:16:56 +0530

    8/5/25

---
+++ BinarySearch/lowerbound.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using  namespace std;
+class Solution{
+public:
+    static int lowerbound(vector<int>& nums, int target){
+        int high = nums.size()-1, low = 0, lbound = nums.size();
+        while(low<=high){
+            int mid = high - (high - low)/2;
+            if(nums[mid] == target){
+                high = mid - 1;
+                lbound = (lbound > mid) ? mid : lbound;
+            }
+            else if(nums[mid] < target)
+                low = mid + 1;
+            else if(nums[mid] > target){
+                high = mid - 1;
+                // if(lbound > nums.size() || nums[lbound] <= nums[mid])
+                lbound = mid;
+            }
+        }
+        return lbound;
+    }
+};
+int main(){
+    vector<int> arr;
+    int range, target;
+    cout << "Enter range : ";
+    cin >> range;
+    for(int i = 0; i<range; i++){
+        int num;
+        cin >> num;
+        // cout << num << "\t";
+        arr.push_back(num);
+    }
+    cout << endl;
+    cout << "Enter target : ";
+    cin >> target; 
+    int ans = Solution :: lowerbound(arr, target);
+    cout << "Ans is : " << ans << endl;
+}
