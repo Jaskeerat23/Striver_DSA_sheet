e54b411ff6d6afd048c340cb96bbfb531f010a71
Jaskeerat23 <jaskeerat23006@gmail.com>
2025-05-10 18:36:35 +0530

    10/5/25

---
+++ BinarySearch/searchrotatedsorted.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+public:
+    static int search(vector<int>& nums, int target){
+        int high = nums.size()-1, low = 0;
+        while(low<=high){
+            int mid = high - (high - low)/2;
+            if(nums[mid] == target)
+                return mid;
+            if(nums[low] <= nums[mid]){
+                if(target >= nums[low] && target < nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            else if(nums[high] >= nums[mid]){
+                if(target <= nums[high] && target > nums[mid])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+int main(){
+    vector<int> arr;
+    int range, target;
+    cout << "Enter range : ";
+    cin >> range;
+    for(int i = 0; i<range; i++){
+        int num;
+        cin >> num;
+        arr.push_back(num);
+    }
+    cout << endl;
+    cout << "Enter target : ";
+    cin >> target; 
+    int ans = Solution :: search(arr, target);
+    cout << "Ans is : " << ans << endl;
+}
