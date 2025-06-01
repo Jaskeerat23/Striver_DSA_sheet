39f787dee4e7c1ae2a2265985460cfeadda2be77
Jaskeerat23 <jaskeerat23006@gmail.com>
2025-06-01 21:07:03 +0530

    1/6/25

--- BinarySearch/findsqrt.cpp
+++ BinarySearch/findsqrt.cpp
@@ -3,17 +3,19 @@ class Solution{
 public:
     static int sqrt(int n){
         if(n == 0 || n == 1) return n;
-        int low = 0, high = n, mid  = -1;
+        int low = 0, high = n, ans = -1;
+        long long mid = -1;
         while(low<=high){
-            mid = high - (high - low)/2;
-            if(mid * mid == n)
-                return mid;
+            long long mid = high - (high - low)/2;
+            long long val = mid * mid;
+            if(val <= (long long)n){
+                ans = mid;
+                low = mid + 1;
+            }
             else if(mid * mid > n)
                 high = mid - 1;
-            else
-                low = mid + 1;
         }
-        return mid - 1;
+        return ans;
     }
 };
 int main(){
