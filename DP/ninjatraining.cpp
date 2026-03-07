830c9843bb5c4c12d4fddb35d58f3f709f00b38c
Jaskeerat Singh <jaskeerat23006@gmail.com>
2026-03-07 08:11:44 +0530

    Ninja Training Memoization/Tabulation/SO

--- DP/ninjatraining.cpp
+++ DP/ninjatraining.cpp
@@ -6,20 +6,20 @@ public:
     //Memoization
     int DPM(vector<vector<int>>& mat, vector<vector<int>>& dp, int day, int last) {
 
-        cout << "Last day i performed " << last << endl;
-
-        if(day >= n) {cout << last << endl; return 0;}
+        if(day >= n) return 0;
 
         if(last > -1 && dp[day][last] != -1) return dp[day][last];
         
         int maxi = 0;
         for(int i = 0; i < 3; i++) {
-            cout << "Inside loop\n";
             if(i != last) maxi = max(maxi, mat[day][i] + DPM(mat, dp, day + 1, i));
 
         }
-
-        return dp[day][last] = maxi;
+        
+        //For first recursive call, we can take a dp of size 4 as 2nd Dim to avoid writing the following conditions
+        if(last > -1)
+            return dp[day][last] = maxi;
+        return maxi;
 
     }
 
@@ -34,32 +34,18 @@ public:
             for(int j = 0; j < 3; j++) {
 
                 for(int today = 0; today < 3; today++) {
-
                     if(j != today)
                         dp[i][today] = max(dp[i][today], mat[i][today] + dp[i + 1][j]);
-
                 }
-
             }
         }
 
-        cout << "Final DP is\n";
-        for(auto& it1 : dp) {
-            for(auto& it2 : it1) 
-                cout << it2 << " ";
-            cout << endl;
-        }
-
-        int maxi = 0;
-        for(int i = 0; i < 3; i++) {
-            maxi = max(maxi, dp[0][i]);
-        }
-        return maxi;
+        return max(dp[0][0], max(dp[0][1], dp[0][2]));
 
     }
 
     //Space Optimization
-int DPTSO(vector<vector<int>>& mat, vector<int>& dp) {
+    int DPTSO(vector<vector<int>>& mat, vector<int>& dp) {
         int n = dp.size();
         for(int i = 0; i < 3; i++) {
             dp[i] = mat[n - 1][i];
@@ -79,19 +65,7 @@ int DPTSO(vector<vector<int>>& mat, vector<int>& dp) {
             dp = temp;
         }
 
-        // cout << "Final DP is\n";
-        // for(auto& it1 : dp) {
-        //     for(auto& it2 : it1) 
-        //         cout << it2 << " ";
-        //     cout << endl;
-        // }
-
-        int maxi = 0;
-        for(int i = 0; i < 3; i++) {
-            maxi = max(maxi, dp[i]);
-        }
-        return maxi;
-
+        return max(dp[0], max(dp[0], dp[0]));
     }
 
     int maximumPoints(vector<vector<int>>& mat) {
